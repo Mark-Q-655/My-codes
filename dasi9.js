dd238f197be60c1c8eeeb330a5988d70d7c06a4b
Mark-Q-655 <budaghyanmark4@gmail.com>
2025-05-05 21:44:21 +0400

    Add files via upload

---
+++ dasi9.js
@@ -0,0 +1,18 @@
+var a = prompt("գրիրր a");
+    var b = prompt("գրիրր b");
+    var c = prompt("գրիրր c");
+    var d = b * b - 4 * a * c;
+    var result = document.getElementById("result");
+
+    if (d < 0) {
+      result.textContent = "no solution";
+    }
+    if (d == 0) {
+      var x = -b / 2 * a;
+      result.textContent = "x=" + x;
+    }
+    if (d > 0) {
+      var x1 = (-1 * b + Math.sqrt(d)) / 2 * a;
+      var x2 = (-1 * b - Math.sqrt(d)) / 2 * a;
+      result.textContent = "x1=" + x1 + " ;" + " x2=" + x2;
+    }
