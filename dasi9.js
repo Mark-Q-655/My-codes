71ee3b0b5e40dd59fb91547f6fd209966d54187b
Mark-Q-655 <budaghyanmark4@gmail.com>
2025-05-06 14:04:01 +0400

    Update dasi9.js

--- dasi9.js
+++ dasi9.js
@@ -1,18 +1,18 @@
-var a = prompt("գրիրր a");
-    var b = prompt("գրիրր b");
-    var c = prompt("գրիրր c");
+var a = prompt("գրիր a");
+    var b = prompt("գրիր b");
+    var c = prompt("գրիր c");
     var d = b * b - 4 * a * c;
     var result = document.getElementById("result");
 
     if (d < 0) {
-      result.textContent = a + "x^2 +" + b + "x" + "+" + "(" + c + ")" + ";<br />" + "D=" + d + " ; " + "no solution";
+      result.textContent = a + "x^2 +" "(" + b ")" + "x" + "+" + "(" + c + ")" + ";<br />" + "D=" + d + " ; " + "no solution";
     }
     if (d == 0) {
       var x = -b / 2 * a;
-      result.textContent = a + "x^2 +" + b + "x" + "+" + "(" + c + ")" + ";<br />" + "D=" + d + " ;\n" + "x=" + x;
+      result.textContent = a + "x^2 +" "(" + b + ")" + "x" + "+" + "(" + c + ")" + ";<br />" + "D=" + d + " ;\n" + "x=" + x;
     }
     if (d > 0) {
       var x1 = (-1 * b + Math.sqrt(d)) / 2 * a;
       var x2 = (-1 * b - Math.sqrt(d)) / 2 * a;
-      result.textContent = a + "x^2 +" + b + "x" + "+" + "(" + c + ")" + ";<br />" + "D=" + d + " ;\n " + "x1=" + x1 + " ; " + " x2=" + x2;
+      result.textContent = a + "x^2 +" + "(" + b + ")" + "x" + "+" + "(" + c + ")" + ";<br />" + "D=" + d + " ;\n " + "x1=" + x1 + " ; " + " x2=" + x2;
     }
