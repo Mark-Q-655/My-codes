b0a37c671743eb08e6ea3a830e7353683945f431
Mark-Q-655 <budaghyanmark4@gmail.com>
2025-05-06 14:07:05 +0400

    Update dasi9.js

--- dasi9.js
+++ dasi9.js
@@ -1,11 +1,11 @@
-var a = prompt("գրիր a");
+    var a = prompt("գրիր a");
     var b = prompt("գրիր b");
     var c = prompt("գրիր c");
     var d = b * b - 4 * a * c;
     var result = document.getElementById("result");
 
     if (d < 0) {
-      result.textContent = a + "x^2 +" "(" + b ")" + "x" + "+" + "(" + c + ")" + ";<br />" + "D=" + d + " ; " + "no solution";
+      result.textContent = a + "x^2 +" "(" + b + ")" + "x" + "+" + "(" + c + ")" + ";<br />" + "D=" + d + " ; " + "no solution";
     }
     if (d == 0) {
       var x = -b / 2 * a;
